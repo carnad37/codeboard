5a2b59191c637ab5f49847aa7e3e5f03170edc1c
Hyeonsu Hwang <java.carnad@gmail.com>
2021-01-31 01:30:28 +0900

    add spring security config

---
+++ codeboard/src/main/java/com/hhs/codeboard/config/security/CustomAuthenticationFilter.java
@@ -0,0 +1,26 @@
+package com.hhs.codeboard.config.security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+	public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+		super.setAuthenticationManager(authenticationManager);
+	}
+	
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		UsernamePasswordAuthenticationToken authRequest = new UsernamePasswordAuthenticationToken("id", "passwd");
+		setDetails(request, authRequest);
+		return this.getAuthenticationManager().authenticate(authRequest);
+	}
+	
+}
