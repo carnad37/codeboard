5bbf7110134951286b740b36eab757dc8b6f13ec
HHS <carnad37@gmail.com>
2021-02-16 15:42:52 +0900

    add memberservice

--- codeboard/src/main/java/com/hhs/codeboard/member/service/MemberService.java
+++ codeboard/src/main/java/com/hhs/codeboard/member/service/MemberService.java
@@ -5,5 +5,5 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import com.hhs.codeboard.jpa.entity.MemberVO;
 
 public interface MemberService extends UserDetailsService {
-	Integer save(MemberVO memberVO);
+	String save(MemberVO memberVO);
 }
