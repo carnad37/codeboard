66566232ceb57c264cb0543ba7a1f0f158cae804
Hyeonsu Hwang <carnad37@gmail.com>
2021-07-12 23:30:40 +0900

    update ajp config

---
+++ codeboard/src/main/java/com/hhs/codeboard/config/web/AJPConfig.java
@@ -0,0 +1,28 @@
+package com.hhs.codeboard.config.web;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AJPConfig {
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createAjpConnector());
+        return tomcat;
+    }
+
+    private Connector createAjpConnector() {
+        Connector ajpConnector = new Connector("AJP/1.3");
+        ajpConnector.setPort(9099);
+        ajpConnector.setSecure(false);
+        ajpConnector.setAllowTrace(false);
+        ajpConnector.setScheme("http");
+        return ajpConnector;
+    }
+
+}
