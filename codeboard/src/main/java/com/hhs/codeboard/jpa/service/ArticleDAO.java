71cef38515a94f39c6b25cdb798f64ada19b39f6
Hyeonsu Hwang <carnad37@gmail.com>
2021-07-21 23:32:19 +0900

    update search function

--- codeboard/src/main/java/com/hhs/codeboard/jpa/service/ArticleDAO.java
+++ codeboard/src/main/java/com/hhs/codeboard/jpa/service/ArticleDAO.java
@@ -1,6 +1,9 @@
 package com.hhs.codeboard.jpa.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,5 +18,7 @@ public interface ArticleDAO extends JpaRepository<BoardArticleEntity, Long> {
 
     Optional<BoardArticleEntity> findBySeqAndRegUserSeqAndDelDateIsNull(Integer seq, Integer regUserSeq);
 
+    @Query(value = "select b from BoardArticleEntity b")
+    Page<BoardArticleEntity> selectArticleList(Pageable pageable);
 
 }
